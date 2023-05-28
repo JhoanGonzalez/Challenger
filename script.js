b6f33522c623567608fc1210fccff6dd2e32a7cd
jhoan <jhoanfran@hotmail.com>
2023-05-28 18:00:52 -0400

    funcion para solo letras minusculas

--- script.js
+++ script.js
@@ -86,3 +86,12 @@
         }
         return desencriptado;
     }
+    function check(e) {
+        tecla = (document.all) ? e.keyCode : e.which;
+        if (tecla == 8) {
+            return true;
+        }
+        patron = /[a-z]/;
+        tecla_final = String.fromCharCode(tecla);
+        return patron.test(tecla_final);
+    }
